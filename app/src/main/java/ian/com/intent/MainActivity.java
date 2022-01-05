6dbbc67350df51e9baebcb2d38a59c2d4f645a4f
n880510n <33122663+n880510n@users.noreply.github.com>
2022-01-05 09:34:25 +0800

    Add files via upload

---
+++ app/src/main/java/ian/com/intent/MainActivity.java
@@ -0,0 +1,49 @@
+package ian.com.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Phone(View view) {
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+"0978355530"));
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    1);
+        } else {
+            try {
+                startActivity(intent);
+            } catch(SecurityException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void Map(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:47.4925,19.0513"));
+        startActivity(intent);
+    }
+
+    public void Chrome(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com.tw"));
+        startActivity(intent);
+    }
+}
